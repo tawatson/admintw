2df3e6c406d3e94ae58c21b682ea0a53c6ab528f
tawatson <tawatson@live.com.au>
2018-06-23 15:16:08 +0800

    Further Bug Fix

--- index.php
+++ index.php
@@ -10,6 +10,9 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 
 require "config.php";
 
+$db->query("SELECT * FROM wa_repos");
+$repos = $db->resultSet();
+
 
 ?>
 <!DOCTYPE html>
@@ -392,7 +395,7 @@ require "config.php";
                         <th>Status</th>
                       </tr>
                     <? foreach ($repos as $repo) {
-                    //  $repoClass = new Tawatson_gitHook($db,$repo['local_dir'],$repo['repo_name']);
+                      $repoClass = new Tawatson_gitHook($db,$repo['local_dir'],$repo['repo_name']);
                       ?>
 
                       <tr>
