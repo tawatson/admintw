c3e3c85005302e94e100de778f9dbe19a60e899c
Tawatson <tawatson@live.com.au>
2018-06-23 17:54:00 +1000

    Test AJAX frontend

--- index.php
+++ index.php
@@ -279,7 +279,7 @@ $repos = $db->resultSet();
                       <h6 class="lh-1">Total Visits</h6></div>
                     <div class="layer w-100">
                       <div class="peers ai-sb fxw-nw">
-                        <div class="peer peer-greed"><span id="sparklinedash"></span></div>
+                        <div class="peer peer-greed">XXX</div>
                         <div class="peer"><span class="d-ib lh-0 va-m fw-600 bdrs-10em pX-15 pY-15 bgc-green-50 c-green-500">+10%</span></div>
                       </div>
                     </div>
