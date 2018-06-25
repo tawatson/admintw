228203b1d8b9be4f1c4abde7bc8556798911c39f
Tawatson <tawatson@live.com.au>
2018-06-25 11:57:46 +1000

    Testing Bundle.js removal

--- index.php
+++ index.php
@@ -691,7 +691,7 @@ foreach ($countryvisits['rows'] as $row) {
   </div>
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
   <script type="text/javascript" src="static/vendor.js"></script>
-  <script type="text/javascript" src="static/bundle.js"></script>
+  <!--<script type="text/javascript" src="static/bundle.js"></script>-->
   <script type="text/javascript" src="static/assets/scripts.js"></script>
   <script type="text/javascript" src="static/jquery-jvectormap-2.0.3.min.js"></script>
   <script>
