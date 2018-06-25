e5685bfc123b745df58ca6191100a171bd068323
Tawatson <tawatson@live.com.au>
2018-06-25 13:24:29 +1000

    Minor bug fix

--- index.php
+++ index.php
@@ -199,7 +199,7 @@ foreach ($countryvisits['rows'] as $row) {
           <ul class="nav-right">
           <li class="dropdown">
               <a href="" class="dropdown-toggle no-after peers fxw-nw ai-c lh-1" data-toggle="dropdown">
-                <div class="peer mR-10"><img class="w-2r bdrs-50p" src="https://api.adorable.io/avatars/237/<? echo $_SESSION['email'];?>.png" alt=""></div>
+                <div class="peer mR-10"><img class="w-2r bdrs-50p" src="https://api.adorable.io/avatars/237/<? echo $_SESSION['username'];?>.png" alt=""></div>
                 <div class="peer"><span class="fsz-sm c-grey-900"><? echo $userInfo['full_name'];?></span></div>
               </a>
               <ul class="dropdown-menu fsz-sm">
