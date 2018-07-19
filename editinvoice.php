2b4d1175b3578945cf3214711a42ccc19b849533
Tawatson <tawatson@live.com.au>
2018-07-19 16:25:47 +1000

    Change for compatibility

--- editinvoice.php
+++ editinvoice.php
@@ -269,7 +269,7 @@ $items = $db->resultSet();
         },
         success: function (response) {
           $(".saveInvoiceItems").text('Saved');
-          window.location = "invoice.php?id=<? echo $invoiceId;?>";
+          window.location.href = "invoice.php?id=<? echo $invoiceId;?>";
         },
         error: function(xhr, status, error) {
           var err = eval("(" + xhr.responseText + ")");
