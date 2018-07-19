bfd9490f45b411833a8d0cf27bb905da73476fb2
Tawatson <tawatson@live.com.au>
2018-07-19 15:15:59 +1000

    Minor fix for webapp

--- static/assets/scripts.js
+++ static/assets/scripts.js
@@ -29,3 +29,13 @@ $(function() {
     });
 
 });
+
+var a=document.getElementsByTagName("a");
+for(var i=0;i<a.length;i++)
+{
+    a[i].onclick=function()
+    {
+        window.location=this.getAttribute("href");
+        return false
+    }
+}
