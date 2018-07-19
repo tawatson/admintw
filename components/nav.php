29d90d9def9c5fea96e37c06f4bbd3f942ddafa2
Tawatson <tawatson@live.com.au>
2018-07-19 16:02:41 +1000

    Forgot to initiate component links with .stay

--- components/nav.php
+++ components/nav.php
@@ -5,14 +5,14 @@
     </ul>
     <ul class="nav-right">
       <li class="dropdown">
-        <a href="" class="dropdown-toggle no-after peers fxw-nw ai-c lh-1" data-toggle="dropdown">
+        <a class="dropdown-toggle no-after peers fxw-nw ai-c lh-1" data-toggle="dropdown">
           <div class="peer mR-10"><span class="fsz-sm c-grey-900"><? echo $userInfo['full_name'];?></span></div>
           <div class="peer"><i class="ti-arrow-circle-down"></i></span></div>
         </a>
         <ul class="dropdown-menu fsz-sm">
-          <li><a href="/settings.php" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700"><i class="ti-settings mR-10"></i> <span>Settings</span></a></li>
+          <li><a href="settings.php" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700 stay"><i class="ti-settings mR-10"></i> <span>Settings</span></a></li>
           <li role="separator" class="divider"></li>
-          <li><a href="logout.php" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700"><i class="ti-power-off mR-10"></i> <span>Logout</span></a></li>
+          <li><a href="logout.php" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700 stay"><i class="ti-power-off mR-10"></i> <span>Logout</span></a></li>
         </ul>
       </li>
     </ul>
