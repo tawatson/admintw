6231dc538ee3210e885d3a4eb3e6788a78d98faf
Tawatson <tawatson@live.com.au>
2018-07-21 23:53:31 +1000

    Readjust something for web app

--- components/webapp.php
+++ components/webapp.php
@@ -1,7 +1,5 @@
-<!-- iPad (Retina, portrait) SPLASHSCREEN-->
-<link href="/static/assets/static/images/web-app-splash-portrait.png" media="(orientation: portrait)" rel="apple-touch-startup-image">
 <!-- iPad (Retina, landscape) SPLASHSCREEN-->
-<link href="/static/assets/static/images/web-app-splash.png" media="(orientation: landscape)" rel="apple-touch-startup-image">
+<link href="/static/assets/static/images/web-app-splash.png"  rel="apple-touch-startup-image">
 <!-- iPad (Retina) ICON-->
 <link href="/static/assets/static/images/web-app.png" sizes="144x144" rel="apple-touch-icon">
 <meta name="apple-mobile-web-app-capable" content="yes"/>
